054eb83a34b7b580227c258d26ecc014b8866966
Chinthaka_W <chinthakaprasad789@gmail.com>
2021-06-13 22:59:11 +0530

    Adding Files

---
+++ routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+//Route::resource('/products',ProductController::class);
+
+
+//public routes
+Route::get('/products',[ProductController::class, 'index']);
+Route::get('/products/{id}',[ProductController::class, 'show']);
+Route::get('/products/search/{name}',[ProductController::class, 'search']);
+
+Route::post('/register',[UserController::class, 'register']);
+Route::post('/login',[UserController::class, 'login']);
+
+//protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products',[ProductController::class, 'store']);
+    Route::put('/products/{id}',[ProductController::class, 'update']);
+    Route::delete('/products/{id}',[ProductController::class, 'destroy']);
+
+    Route::post('/logout',[UserController::class, 'logout']);
+});
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
